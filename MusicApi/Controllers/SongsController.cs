637b83971ab2d28481e00c96c8081e6b7c3f3b75
Miguel Raposo <mickaelslb@gmail.com>
2024-12-08 20:12:46 +0000

    Add CRUD methods for SongsController

--- MusicApi/Controllers/SongsController.cs
+++ MusicApi/Controllers/SongsController.cs
@@ -9,9 +9,9 @@ namespace MusicApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SongController : ControllerBase
+    public class SongsController : ControllerBase
     {
-        private List<Song> songs = new List<Song>()
+        private static List<Song> songs = new List<Song>()
         {
             new Song(){Id=0, Title="Willow", Language ="English" },
             new Song(){Id=1, Title="After Glow", Language ="English"}
@@ -19,5 +19,22 @@ namespace MusicApi.Controllers
         [HttpGet]
         public IEnumerable<Song> Get()
         { return songs; }
+
+        [HttpPost]
+        public void Post([FromBody]Song song)
+        {
+            songs.Add(song);
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Song song)
+        {
+            songs[id] = song;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            songs.RemoveAt(id);
+        }
+            
     }
 }
