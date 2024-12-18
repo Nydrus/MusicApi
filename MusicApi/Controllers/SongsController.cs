d297065c4523b0268e189c3a63a1d9cfeccc6eef
Miguel Raposo <mickaelslb@gmail.com>
2024-12-18 20:40:13 +0000

    Update SongsController

--- MusicApi/Controllers/SongsController.cs
+++ MusicApi/Controllers/SongsController.cs
@@ -1,10 +1,9 @@
-﻿using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MusicApi.Data;
 using MusicApi.Helpers;
 using MusicApi.Models;
-using System.IO;
+using System;
 using System.Threading.Tasks;
 
 namespace MusicApi.Controllers
@@ -23,22 +22,13 @@ namespace MusicApi.Controllers
         {
             var imageUrl = await FileHelper.UploadImage(song.Image);
             song.ImageUrl = imageUrl;
+          var audioUrl = await FileHelper.UploadFile(song.AudioFile);
+            song.AudioUrl = audioUrl;
+           song.UploadedDate = DateTime.Now;
             await _dbContext.Songs.AddAsync(song);
             await _dbContext.SaveChangesAsync();
             return StatusCode(StatusCodes.Status201Created);
         }
-        public static async Task<string> UploadImage(IFormFile file)
-        {
-            string connectionString = @"DefaultEndpointsProtocol=https;AccountName=musicblobstorage;AccountKey=U/YdF93ZRP+sZITZmAdpLqAy1Ek1zYg8cfySWLE72cnXyrZsHk7P4Trq45n3JzMm6wp/GUdx23Vw+AStvPYMVA==;EndpointSuffix=core.windows.net";
-            string containerName = "audiofiles";
 
-            BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
-            BlobClient blobClient = blobContainerClient.GetBlobClient(file.FileName);
-            var memoryStream = new MemoryStream();
-            await file.CopyToAsync(memoryStream);
-            memoryStream.Position = 0;
-            await blobClient.UploadAsync(memoryStream);
-            return blobClient.Uri.AbsoluteUri;
-        }
     }
 }
--- MusicApi/Helpers/FileHelper.cs
+++ MusicApi/Helpers/FileHelper.cs
@@ -22,5 +22,18 @@ namespace MusicApi.Helpers
             await blobClient.UploadAsync(memoryStream);                
             return blobClient.Uri.AbsoluteUri;
         }
+        public static async Task<string> UploadFile(IFormFile file)
+        {
+            string connectionString = @"DefaultEndpointsProtocol=https;AccountName=musicblobstorage;AccountKey=U/YdF93ZRP+sZITZmAdpLqAy1Ek1zYg8cfySWLE72cnXyrZsHk7P4Trq45n3JzMm6wp/GUdx23Vw+AStvPYMVA==;EndpointSuffix=core.windows.net";
+            string containerName = "audiofiles";
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(file.FileName);
+            var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            await blobClient.UploadAsync(memoryStream);
+            return blobClient.Uri.AbsoluteUri;
+        }
     }
 }
